c44a6fe27a9eac93afc9ca949d51743c0ec173f3
DJPhelps1 <46822189+DJPhelps1@users.noreply.github.com>
2023-11-04 13:10:12 -0600

    API, Unit, and Integration projects added

---
+++ ShellAndNecklaceIntegrations/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShellAndNecklaceIntegrations
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ShellAndNecklaceUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShellAndNecklaceUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
