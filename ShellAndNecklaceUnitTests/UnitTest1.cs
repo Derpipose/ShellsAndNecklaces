7300c9b03c9cf15b52176f80a98c66b4611020cd
Zane-Clegg <zanerclegg@gmail.com>
2023-11-16 16:38:59 -0700

    Changed page redirects in MainLayout Testing doesn't run yet

--- ShellAndNecklaceUnitTests/UnitTest1.cs
+++ ShellAndNecklaceUnitTests/UnitTest1.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using ShellsAndNecklacesApp.Pages;
+using ShellsAndNecklacesApp.Shared;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Navigations;
 
 namespace ShellAndNecklaceUnitTests
 {
@@ -6,8 +11,10 @@ namespace ShellAndNecklaceUnitTests
 	{
 		public BlazorUnitTestContext()
 		{
-			//register services here:
-
+			var builder = WebApplication.CreateBuilder();
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjgxMzAwMUAzMjMzMmUzMDJlMzBVOHZLME1sZWpxVDRheHphTmozL2J4dm1seEpxVXc3cXh3dmhBRkVpSEFBPQ ==");
+			builder.Services.AddServerSideBlazor();
+			builder.Services.AddSyncfusionBlazor();
 			//var mockService = new Mock<IService>();
 			//mockService.Setup(m=>m.DoSomething()).Returns(5);
 			//Services.AddScoped<IService>(_ => mockService.Object);
@@ -28,7 +35,21 @@ namespace ShellAndNecklaceUnitTests
 
         [Fact]
         public void ButtonsRedirectCorrectly() {
-            var cut = RenderComponent<ShellsAndNecklacesApp.Pages.Index>();
-        }
+			//arrange
+			using var ctx = new TestContext();
+			// Add Syncfusion Blazor service.
+			ctx.Services.AddSyncfusionBlazor();
+			ctx.Services.AddOptions();
+
+			var cut = RenderComponent<MainLayout>();
+			//	var sfbutton = cut.FindComponent<MenuItem>;
+			var nav = ctx.Services.GetRequiredService<NavigationManager>();
+			
+			//act
+			cut.Find(".e-menu-header .e-menu-item:nth-child(1)").Click();
+			
+			//assert
+			Assert.Equal($"{nav.BaseUri}/review", nav.Uri);
+		}
     }
 }
