950f2e34514b5020fda0f62e55aee06d8fdcb920
Derpipose <thefluffmaster2000@gmail.com>
2023-11-18 13:22:56 -0700

    Took out the Sync Fusion Crap.

--- ShellAndNecklaceUnitTests/UnitTest1.cs
+++ ShellAndNecklaceUnitTests/UnitTest1.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using ShellsAndNecklacesApp.Pages;
 using ShellsAndNecklacesApp.Shared;
-using Syncfusion.Blazor;
-using Syncfusion.Blazor.Navigations;
 
 namespace ShellAndNecklaceUnitTests
 {
@@ -11,10 +9,10 @@ namespace ShellAndNecklaceUnitTests
 	{
 		public BlazorUnitTestContext()
 		{
-			var builder = WebApplication.CreateBuilder();
-			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjgxMzAwMUAzMjMzMmUzMDJlMzBVOHZLME1sZWpxVDRheHphTmozL2J4dm1seEpxVXc3cXh3dmhBRkVpSEFBPQ ==");
-			builder.Services.AddServerSideBlazor();
-			builder.Services.AddSyncfusionBlazor();
+			//var builder = WebApplication.CreateBuilder();
+			//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjgxMzAwMUAzMjMzMmUzMDJlMzBVOHZLME1sZWpxVDRheHphTmozL2J4dm1seEpxVXc3cXh3dmhBRkVpSEFBPQ ==");
+			//builder.Services.AddServerSideBlazor();
+			//builder.Services.AddSyncfusionBlazor();
 			//var mockService = new Mock<IService>();
 			//mockService.Setup(m=>m.DoSomething()).Returns(5);
 			//Services.AddScoped<IService>(_ => mockService.Object);
@@ -38,7 +36,7 @@ namespace ShellAndNecklaceUnitTests
 			//arrange
 			using var ctx = new TestContext();
 			// Add Syncfusion Blazor service.
-			ctx.Services.AddSyncfusionBlazor();
+			//ctx.Services.AddSyncfusionBlazor();
 			ctx.Services.AddOptions();
 
 			var cut = RenderComponent<MainLayout>();
