a51dd5532d41fbe43174b132e3c7222f88e6d218
Derpipose <thefluffmaster2000@gmail.com>
2023-12-11 13:09:37 -0700

    Started tests

---
+++ ShellAndNecklaceUnitTests/ItemSerivceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using ShellAndNecklaceAPI.Services;
+using ShellAndNecklaceAPI.Data;
+using ShellAndNecklaceAPI.Data.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq.EntityFrameworkCore;
+using Moq;
+
+namespace ShellAndNecklaceUnitTests
+{
+
+	public class TestingItemController {
+		private Mock<Logger<ItemService>> MockLogger;
+		private Mock<OneShotShopContext> MockContext;
+		private ItemService _itemService;
+
+		public TestingItemController() { 
+			MockLogger = new Mock<Logger<ItemService>>();
+			MockContext = new Mock<OneShotShopContext>();
+			_itemService = new ItemService(MockLogger.Object, MockContext.Object);
+		}
+		
+	}
+
+}
